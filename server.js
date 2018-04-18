83c27c80cd6b6f9d4760c690306f8c12cc0c6900
unknown <dxb.dina@gmail.com>
2018-04-18 14:14:40 +0400

    Add project page

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
     res.render('maintenance.hbs', {
-        pageTitle: 'Under Maintenance',
+        pageTitle: 'Maintenance Page',
         welcomeMsg: `Our website is under maintenance`,
         body: `We will be back soon.`,
         currentYear: new Date().getFullYear()
@@ -32,49 +32,43 @@ app.use((req, res, next) => {
    // console.log(req);
     next();
 });
+// helpers
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
 });
-
 hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 });
 // set up handler to https request
 app.get('/', (req, res) => {
-    //   res.send(`Deena's First NodeJs App!`);
-    //   res.send({
-    //       name: 'Deena',
-    //       likes: [
-    //           'coffee',
-    //           'code',
-    //           'traveling'
-    //       ],
-    //       slang: 'Express is Owesome!'
-    //   });
-
     res.render('home.hbs', {
         pageTitle: 'Home Page',
         welcomeMsg: `Welcome to Dina Samy's website`,
-        body: `Home Page of new website Technology used is express nodejs`,
-        currentYear: new Date().getFullYear()
+        body: `Home Page of new website Technology used is express nodejs`
     });
 });
-
+//about
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
         welcomeMsg: `About Us`,
-        body: `This is an about us page`,
-        currentYear: new Date().getFullYear()
+        body: `This is an about us page`
     });
 });
-
+// bad request
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to fulfill your request'
     });
 });
-
+// projects page
+app.get('/projects',(req, res) => {
+    res.render('projects.hbs',{
+        pageTitle: 'Projects',
+        welcomeMsg: `My portfolio`,
+        body: `My online portfolio.`
+    });
+});
 // bind the app to port
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
